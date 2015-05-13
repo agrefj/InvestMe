f9882e439f60519ec8d537d4515854db5d25d448
agrefj <agrefj@gmail.com>
2015-05-14 00:42:25 +0300

    1.2

--- app/src/main/java/ru/getlect/investme/investme/MainActivity.java
+++ app/src/main/java/ru/getlect/investme/investme/MainActivity.java
@@ -4,11 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,27 +19,6 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
-
-        //Fake data for test
-        String[] ratingArray = {
-                "Мкб - Мультивалютный - 18%",
-                "Сбер - Как всегда лучший ваще - 2%",
-                "Альфа - Для самцов - 14%",
-                "Банк деревни Кукуево - Все для своих - 64%"
-        };
-
-        List<String> depositRating = new ArrayList<String>(Arrays.asList(ratingArray));
-
-        mRatingAdapter = new ArrayAdapter<String>(
-                //current context
-                getActivity(),
-                //id of list item layout
-                R.id.listview_rating;
-                //id of text view
-
-
-        )
-
 
         return true;
     }
