d185865ede30bcb018f740b6e3a39eb5507023de
agrefj <agrefj@gmail.com>
2015-05-14 18:59:32 +0300

    contract added

---
+++ app/src/main/java/ru/getlect/investme/investme/data/InvestMeContract.java
@@ -0,0 +1,11 @@
+package ru.getlect.investme.investme.data;
+
+/**
+ * Created by fj on 14.05.2015.
+ */
+public class InvestMeContract {
+
+    public static final String DEPOSITS_URL = "http://159.253.23.26/DepositsQuery.json";
+
+
+}
